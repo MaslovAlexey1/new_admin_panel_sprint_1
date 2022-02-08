4330ac250b9ab4e508de8dd446156b9e82151ca5
Alexey Maslov <maslov.msu@gmail.com>
2022-02-08 14:38:13 +0300

    (fix) 02_movie_admin after review_1

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -27,6 +27,7 @@ class Genre(UUIDMixin, TimeStampedMixin):
         db_table = "content\".\"genre"
         verbose_name = _('genre')
         verbose_name_plural = _('genres')
+        models.UniqueConstraint(fields=['name'], name='unique_genre_name')
 
     def __str__(self):
         return self.name
@@ -40,6 +41,7 @@ class Person(UUIDMixin, TimeStampedMixin):
         db_table = "content\".\"person"
         verbose_name = _('actor')
         verbose_name_plural = _('actors')
+        models.UniqueConstraint(fields=['full_name', 'birth_date'], name='unique_full_name_birth_date')
 
     def __str__(self):
         return self.full_name
@@ -71,6 +73,7 @@ class Filmwork(UUIDMixin, TimeStampedMixin):
         db_table = "content\".\"film_work"
         verbose_name = _('filmwork')
         verbose_name_plural = _('filmworks')
+        models.UniqueConstraint(fields=['title', 'creation_date', 'type'], name='unique_title_creation_date_type')
 
     def __str__(self):
         return self.title
@@ -83,13 +86,31 @@ class GenreFilmwork(UUIDMixin):
 
     class Meta:
         db_table = "content\".\"genre_film_work"
+        models.UniqueConstraint(fields=['film_work_id', 'genre_id'], name='unique_film_work_genre')
+        indexes = [
+            models.Index(fields=['film_work_id']),
+            models.Index(fields=['genre_id']),
+        ]
 
 
 class PersonFilmwork(UUIDMixin):
+    class RoleType(models.TextChoices):
+        ACTOR = 'actor', _('Actor')
+        DIRECTOR = 'director', _('Director')
+        WRITER = 'writer', _('Writer')
+
     film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE, blank=False)
     person = models.ForeignKey('Person', on_delete=models.CASCADE, blank=False)
-    role = models.TextField(_('role'), blank=False)
+    role = models.CharField(_('role'), 
+                            blank=False,
+                            max_length=32,
+                            choices=RoleType.choices,)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "content\".\"person_film_work"
+        models.UniqueConstraint(fields=['film_work_id', 'person_id', 'role'], name='unique_film_work_person_role')
+        indexes = [
+            models.Index(fields=['film_work_id']),
+            models.Index(fields=['person_id']),
+        ]
